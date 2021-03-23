d9f64287541a5c13ab7d3a2c293b716aa2e4ff93
Tara4 <tararlee2000@gmail.com>
2021-03-23 13:05:55 +1300

    Add Project

---
+++ MyRecipes/Notes/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string CreatedBy { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+    }
+}
